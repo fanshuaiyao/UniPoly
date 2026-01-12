b47ca7271dd96dcdb977ac3bf8db60c6f5a7df23
fanshuaiyao <1345822123@qq.com>
2026-01-12 19:21:01 +0800

    hotmap

--- scripts/run_pretrain.sh
+++ scripts/run_pretrain.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 export PYTHONPATH=$(pwd)
 export CUDA_VISIBLE_DEVICES=0
+
 nohup python scripts/pretrain.py \
-  --dataset_name smi_test \
+  --dataset_name smi_all \
   --modalities smiles fp text graph \
-  --epochs 5 \
-  --batch_size 2 \
+  --epochs 20 \
+  --batch_size 16 \
   --lr 5e-5 \
   > ./logs/pretrain_sftg_unfreeze.log 2>&1 &
-
