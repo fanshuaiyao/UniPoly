c96e222d253bc8ed7e30e6c4463f012f1dc97334
fanshuaiyao <1345822123@qq.com>
2026-02-07 15:53:57 +0800

    4090

--- scripts/run_pretrain.sh
+++ scripts/run_pretrain.sh
@@ -4,8 +4,8 @@ export CUDA_VISIBLE_DEVICES=0
 
 nohup python scripts/pretrain.py \
   --dataset_name smi_all \
-  --modalities smiles fp text graph \
-  --epochs 20 \
+  --modalities smiles fp text graph\
+  --epochs 10 \
   --batch_size 16 \
   --lr 5e-5 \
-  > ./logs/pretrain_sftg_unfreeze.log 2>&1 &
+  > ./logs/pretrain_sftg_unfreeze_5.log 2>&1 &
