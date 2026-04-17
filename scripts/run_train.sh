1275145e71f48e7091bb86b7c668ee72b4787dc3
fanshuaiyao <1345822123@qq.com>
2026-04-17 21:30:29 +0800

    100

--- scripts/run_train.sh
+++ scripts/run_train.sh
@@ -6,10 +6,10 @@ export CUDA_VISIBLE_DEVICES=0
 
 nohup python -u scripts/train.py \
   --modalities smiles fp text graph \
-  --tasks clintox \
+  --tasks lipo \
   --pretrained_model_path ./pretrained_models/saved_pretrained_model.pth \
   --batch_size 16 \
-  --epochs 1 \
+  --epochs 100 \
   --patience 5 \
   --freeze_encoder \
-  > ./logs/train_test.log 2>&1 &
+  > ./logs/train_.log 2>&1 &
